870e45679fae5a8f005ba93c558815d221ff47d8
큐아 <QUAH@simgyuhyeon-ui-MacBookPro.local>
2024-06-13 02:11:19 +0900

    verifying score

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -7,6 +7,8 @@ import { getGameAssets } from '../init/assets.js';
 export const moveStageHandler = (userId, payload) => {
   // 유저의 현재 스테이지 배열을 가져오고, 최대 스테이지 ID를 찾는다.
   let currentStages = getStage(userId);
+  const { stages } = getGameAssets();
+
   if (!currentStages.length) {
     return { status: 'fail', message: 'No stages found for user' };
   }
@@ -22,19 +24,19 @@ export const moveStageHandler = (userId, payload) => {
 
   // 점수 검증
   const serverTime = Date.now();
-  const elapsedTime = (serverTime - currentStage.timestamp) / 1000; // 초 단위로 계산
-
-  // 1초당 1점, 100점이상 다음스테이지 이동, 오차범위 5
-  // 클라이언트와 서버 간의 통신 지연시간을 고려해서 오차범위 설정
-  // elapsedTime 은 100 이상 105 이하 일 경우만 통과
-  if (elapsedTime < 10 || elapsedTime > 10.5) {
-    return { status: 'fail', message: 'Invalid elapsed time' };
-  }
 
   // 게임 에셋에서 다음 스테이지의 존재 여부 확인
-  const { stages } = getGameAssets();
-  if (!stages.data.some((stage) => stage.id === payload.targetStage)) {
+  const nextStage = stages.data.find((stage) => stage.id === payload.targetStage);
+  if (!nextStage) {
     return { status: 'fail', message: 'Target stage does not exist' };
+  }
+
+  // 점수 검증
+  const requiredScore = nextStage.score;
+
+  // 오차범위 5점
+  if (payload.score < requiredScore - 5 || payload.score > requiredScore + 5) {
+    return { status: 'fail', message: 'Invalid score' };
   }
 
   // 유저의 다음 스테이지 정보 업데이트 + 현재 시간

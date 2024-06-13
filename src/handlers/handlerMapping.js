acc418fbdbc30f8c7286fdb649e1bad29365cb06
큐아 <QUAH@simgyuhyeon-ui-MacBookPro.local>
2024-06-14 00:58:16 +0900

    added sendEvent for itemHandler

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -2,11 +2,13 @@
 
 import { gameEnd, gameStart } from "./game.handler.js";
 import { moveStageHandler } from "./stage.handler.js";
+import { getItemHandler } from "./item.handler.js";
 
 const handlerMappings={
     2:gameStart,
     3:gameEnd,
     11:moveStageHandler,
+    12:getItemHandler,
 };
 
 export default handlerMappings;
